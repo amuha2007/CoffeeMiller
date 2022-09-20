1b2bb180f2d79ef97beb64515d88d3f4b870724c
Artem Mukha <73305145+amuha2007@users.noreply.github.com>
2022-09-20 20:41:35 +0300

    Update cardOpenCoffee.js

--- modules/cardOpenCoffee.js
+++ modules/cardOpenCoffee.js
@@ -385,11 +385,13 @@ export function cardOpenCoffee(cardCoffeeInfo){
         <p class='AdditioallyItemText'>${cardCoffeeInfo.Additionally[i].text}</p>
         
         `;
+        if(cardCoffeeInfo.Additionally[i].img!==" "||cardCoffeeInfo.Additionally[i].img!==undefined){
         let AdditionallyImg=document.createElement('img');
         AdditionallyImg.setAttribute('src',`${cardCoffeeInfo.Additionally[i].img}`);
         AdditionallyImg.setAttribute('class','AdditioallyItemImg');
         AdditionallyItem.appendChild(AdditionallyImg);
         document.querySelector('.AdditionallyBody').appendChild(AdditionallyItem);
+        }
     }
 
     
