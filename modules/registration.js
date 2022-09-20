33031e8152c778c59059bd8d168f50e710d3480f
Artem Mukha <73305145+amuha2007@users.noreply.github.com>
2022-09-21 00:30:46 +0300

    Update registration.js

--- modules/registration.js
+++ modules/registration.js
@@ -20,14 +20,14 @@ export function registration(element){
             modalWindow.innerHTML=`
                 <div class='modalWindowContainerFront'>
                     <div class='modalWindowContainerLeft'>
-                        <img src='/img/coffee-breans&cup2.png' style='width:220px'> 
+                        <img src='./img/coffee-breans&cup2.png' style='width:220px'> 
                         <p  class='modalWindowHeaderText'>Добро пожаловать!</p>
                         <p>Уже есть аккаунт?</p>
                         <button class='modalWindowButton'>Войти</button>
                     </div>
 
                     <div class='modalWindowContainerRight'>
-                        <img src='/img/Logo.png' style='height:40px'>
+                        <img src='./img/Logo.png' style='height:40px'>
                         <p style='font-family: Ubuntu, sans-serif; font-style: normal;font-weight: 900;font-size: 25px; margin:10px;'>Регистрация</p>
                         <p id='modalWindowText' style='text-align:center;font-family: Ubuntu, sans-serif;font-size:14px;'>Зарегистрируйтесь на сайте, чтобы первым получать скидки <br>и узнавать акционные предложения!</p>
                         <input class='modalWindowContainerRightInput' placeholder='Имя и фамилия'></input>
@@ -37,17 +37,17 @@ export function registration(element){
                         <button id='modalWindowregistrationButton'>Регистрация</button>
                         
                     </div>
-                    <div id='modalWindowCloseContainer'><img src='/img/icons/outline_close_black_24dp.png' id='modalWindowClose'></div>
+                    <div id='modalWindowCloseContainer'><img src='./img/icons/outline_close_black_24dp.png' id='modalWindowClose'></div>
                 </div>
                 <div class='modalWindowContainerBack'>
                     <div class='modalWindowContainerLeft'>
-                    <img src='/img/coffee-breans&cup2.png' style='width:220px'>
+                    <img src='./img/coffee-breans&cup2.png' style='width:220px'>
                     
                     <p style='text-align: center;'>Получайте скидки первым</p>
                     <button class='modalWindowButton' id='modalWindowButtonRegistation'>Регистрация</button>
                     </div>
                     <div class='modalWindowContainerRight'>
-                        <img src='/img/Logo.png' style='height:40px'>
+                        <img src='./img/Logo.png' style='height:40px'>
                         <p style='font-family: Ubuntu, sans-serif; font-style: normal;font-weight: 900;font-size: 25px; margin:10px;'>Войти в личный кабинет</p>
                         
                         
@@ -57,14 +57,12 @@ export function registration(element){
                         <button id='modalWindowregistrationButton'>Войти</button>
                         <a href id='fogetPassword'>забыли пароль?</a>
                     </div>
-                    <div ><img src='/img/icons/outline_close_black_24dp.png' id='modalWindowCloseRegistration'></div>
+                    <div ><img src='./img/icons/outline_close_black_24dp.png' id='modalWindowCloseRegistration'></div>
                 </div>
             `;
             
             modalWrapper.appendChild(modalWindow);
-            //modalWrapper.style.opacity=1;
-            //добавил div на весь экран 
-            //теперь надо плавно затемнить его //
+            
             setTimeout(() => {
                 modalWrapper.style.background='rgba(0,0,0,.7)';
 
@@ -73,17 +71,16 @@ export function registration(element){
                 console.log(document.querySelector('.modalWindowContainerFront'));
                 document.querySelectorAll('#modalWindowClose').forEach(function(elem){
                     elem.addEventListener('click', function(){
-                        //сделай с сет таймаутом что бы окно становилось прозрачным а фон позади вместо черного тоже становился прозрачным , а потмо через время на анимацию ты удалишь елемент
-                        //modalWindow.style.opacity='0';
+                        
                         modalWrapper.style.background='rgba(0,0,0,0)';
                         console.log(document.querySelector('.modalWindowContainerFront'));
                         document.querySelector('.modalWindowContainerFront').style.cssText='opacity:0';
-                        document.querySelector('.modalWindowContainerBack').style.cssText='opacity:0';//тут проработать что бы он не поворачивался по новой
+                        document.querySelector('.modalWindowContainerBack').style.cssText='opacity:0';
                         setTimeout(()=>{
                             modalWrapper.remove();
                             
                         },1000);
-                        //надо сделать что бы модальное окно менялось при нажатии на регестрацию или просто войти
+                        
                     })
                 })
 
@@ -107,7 +104,6 @@ export function registration(element){
                 document.querySelector('.modalWindowContainerBack').style.transform='rotateY(360deg)';
                 document.querySelector('.modalWrapper').style.background='rgba(0,0,0,0)';
                 
-                        //document.querySelector('.modalWindowContainerFront').style.cssText='opacity:0';
                         document.querySelector('.modalWindowContainerBack').style.opacity='0';
 
                 setTimeout(() => {
@@ -118,13 +114,13 @@ export function registration(element){
             
         });
 
-// можно еще было поработать с асинхронными функциями 
+
 let ForgetPassword=function(event){
     event.preventDefault();
     document.querySelectorAll('.modalWindowContainerRight')[1].style.opacity='0';
     setTimeout(() => {
         document.querySelectorAll('.modalWindowContainerRight')[1].innerHTML=`
-                    <img src='/img/Logo.png' style='height:40px'>
+                    <img src='./img/Logo.png' style='height:40px'>
                     <p style='font-family: Ubuntu, sans-serif; font-style: normal;font-weight: 900;font-size: 25px; margin:10px;'>Забыли пароль?</p>
                     <input type='Email' class='modalWindowContainerRightInput' placeholder='Email'></input>
                     <button id='modalWindowregistrationButtonRecoveryCod'>Получить код</button>
@@ -140,7 +136,7 @@ let ForgetPassword=function(event){
                     document.querySelectorAll('.modalWindowContainerRight')[1].style.opacity='0';
                     setTimeout(() => {
                         document.querySelectorAll('.modalWindowContainerRight')[1].innerHTML=`
-                            <img src='/img/Logo.png' style='height:40px'>
+                            <img src='./img/Logo.png' style='height:40px'>
                             <p style='font-family: Ubuntu, sans-serif; font-style: normal;font-weight: 900;font-size: 25px; margin:10px;'>Войти в личный кабинет</p>
                             <input type='Email' class='modalWindowContainerRightInput' placeholder='Email'></input>
                             <input class='modalWindowContainerRightInput' placeholder='Пароль'></input>
@@ -158,9 +154,9 @@ let ForgetPassword=function(event){
                 
     }, 1000);   
     
-}// end function ForgetPasword
+}
 
-//вызов функции для кнопки forgetPassword
+
 document.querySelector('#fogetPassword').addEventListener('click', ForgetPassword)
 
 
