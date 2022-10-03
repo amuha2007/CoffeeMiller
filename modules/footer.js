1b3502b8baccc383e32951cde1492add3417fc5f
Artem Mukha <73305145+amuha2007@users.noreply.github.com>
2022-10-03 20:12:23 +0300

    Update footer.js

--- modules/footer.js
+++ modules/footer.js
@@ -11,7 +11,7 @@ export function Footer(){
                 <div class='footerNavigation'>
                     <div class="logo"><img src="./img/Logo.png"></div>
                     <div class="navButtons">
-                    <a href='#'>Каталог товаров</a>
+                    
                     <a href='#'>Блог</a>
                     <a href='#'>Контакты</a>
                 </div> 
