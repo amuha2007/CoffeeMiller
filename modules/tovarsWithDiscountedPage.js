5a0f1d7bbf8fea010ed209cf2c903e2166f5c06b
Artem Mukha <73305145+amuha2007@users.noreply.github.com>
2022-10-03 20:29:46 +0300

    Update tovarsWithDiscountedPage.js

--- modules/tovarsWithDiscountedPage.js
+++ modules/tovarsWithDiscountedPage.js
@@ -14,6 +14,7 @@ import navigationsA from '../pages/pagesModules/navigationsA.js';
 
 
 export function tovarsWithdiscountedPage(event){
+    document.querySelector('.navButtonsLinks').classList.remove('visibility');
     window.scrollTo(0, 0);
     
     document.querySelector('.main').classList.add('mainHeight');
